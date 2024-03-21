6559aaa8a078b48c7a5a5397fd9b0410cbf9e05d
Saurav S <77258135+sauravss98@users.noreply.github.com>
2024-03-21 21:29:59 +0000

    Staff class added

--- Group14-A2/src/main/java/User/Staff.java
+++ Group14-A2/src/main/java/User/Staff.java
@@ -1,5 +1,23 @@
 package User;
 
-public class Staff {
+public class Staff extends User{
+    private int staffID;
+    private int hoursWorked;
+    private int totalHours;
 
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public int getTotalHours() {
+        return totalHours;
+    }
+
+    public void setHoursWorked(Integer hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public void setTotalHours(Integer totalHours) {
+        this.totalHours = totalHours;
+    }
 }
