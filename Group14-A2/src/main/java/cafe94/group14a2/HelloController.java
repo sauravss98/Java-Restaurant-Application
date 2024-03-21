0d1a62d96ace2683bfd7474cb6b13db695979001
Saurav S <77258135+sauravss98@users.noreply.github.com>
2024-03-21 20:44:09 +0000

    first commit

---
+++ Group14-A2/src/main/java/cafe94/group14a2/HelloController.java
@@ -0,0 +1,14 @@
+package cafe94.group14a2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ Group14-A2/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module cafe94.group14a2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens cafe94.group14a2 to javafx.fxml;
+    exports cafe94.group14a2;
+}
