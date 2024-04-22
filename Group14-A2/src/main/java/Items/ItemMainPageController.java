354dcac713aef3e43cef979fa2ac62f093e27aed
Saurav S <77258135+sauravss98@users.noreply.github.com>
2024-04-22 18:12:34 +0530

    added functionality for back button

--- Group14-A2/src/main/java/Items/ItemMainPageController.java
+++ Group14-A2/src/main/java/Items/ItemMainPageController.java
@@ -1,11 +1,13 @@
 package Items;
 
 import User.Manager;
+import cafe94.group14a2.Main;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ItemMainPageController {
@@ -23,7 +25,19 @@ public class ItemMainPageController {
 
     public void initialize(){
         refreshItemList();
+        backButton.setOnAction(e->{
+            handleBackButtonClick();
+        });
     }
+
+    private void handleBackButtonClick() {
+        try {
+            Main.setRoot("managerMainPage");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     private void refreshItemList() {
         // Clear the displayed list
