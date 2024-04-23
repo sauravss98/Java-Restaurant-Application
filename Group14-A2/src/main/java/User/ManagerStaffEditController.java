c173fff09282f6a69d19456cc0dcebb8c859d778
Saurav S <77258135+sauravss98@users.noreply.github.com>
2024-04-23 23:12:13 +0100

    added the feature for manager staff edit page

--- Group14-A2/src/main/java/User/ManagerStaffEditController.java
+++ Group14-A2/src/main/java/User/ManagerStaffEditController.java
@@ -1,5 +1,10 @@
 package User;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -15,6 +20,16 @@ public class ManagerStaffEditController {
     private Manager activeManager;
     private Waiter activeWaiter;
     private Driver activeDriver;
+    @FXML private Label idLabel;
+    @FXML private Label userNameLabel;
+    @FXML private Label userTypeLabel;
+    @FXML private Spinner workedHoursSpinner;
+    @FXML private Spinner totalHoursSpinner;
+    @FXML private Button removeStaffButton;
+    @FXML private Button cancelButton;
+    @FXML private Button confirmStaffButton;
+
+
 
     public ManagerStaffEditController(){}
 
@@ -65,17 +80,91 @@ public class ManagerStaffEditController {
 
     public void setActiveChef(Chef activeChef) {
         this.activeChef = activeChef;
+        initialize();
     }
 
     public void setActiveDriver(Driver activeDriver) {
         this.activeDriver = activeDriver;
+        initialize();
     }
 
     public void setActiveManager(Manager activeManager) {
         this.activeManager = activeManager;
+        initialize();
     }
 
     public void setActiveWaiter(Waiter activeWaiter) {
         this.activeWaiter = activeWaiter;
+        initialize();
+    }
+
+    public void initialize(){
+        refreshLabelData();
+        if(activeManager!=null) {
+            System.out.println("active user is " + activeManager.getFullName());
+        }
+        cancelButton.setOnAction(e->{
+            handleCancelClick();
+        });
+    }
+
+    private void handleCancelClick() {
+        if(stage!=null){
+            stage.close();
+        }
+    }
+
+    private void refreshLabelData() {
+        if(activeStaff!=null){
+            if(activeStaff.getUserType().equals("Manager")){
+                setDataLabels(activeManager);
+            }
+            if(activeStaff.getUserType().equals("Chef")){
+                setDataLabels(activeChef);
+            }
+            if(activeStaff.getUserType().equals("Driver")){
+                setDataLabels(activeDriver);
+            }
+            if(activeStaff.getUserType().equals("Waiter")){
+                setDataLabels(activeWaiter);
+            }
+        }
+    }
+
+    private void setDataLabels(Manager manager){
+        idLabel.setText("Staff ID: "+manager.getUserId());
+        userNameLabel.setText("Name: "+manager.getFullName());
+        userTypeLabel.setText("User Type: "+manager.getUserType());
+        SpinnerValueFactory<Integer> workHouredValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE, manager.getHoursWorked());
+        workedHoursSpinner.setValueFactory(workHouredValueFactory);
+        SpinnerValueFactory<Integer> totalHoursValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE, manager.getTotalHours());
+        totalHoursSpinner.setValueFactory(totalHoursValueFactory);
+    }
+    private void setDataLabels(Chef chef){
+        idLabel.setText("Staff ID: "+chef.getUserId());
+        userNameLabel.setText("Name: "+chef.getFullName());
+        userTypeLabel.setText("User Type: "+chef.getUserType());
+        SpinnerValueFactory<Integer> workHouredValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE, chef.getHoursWorked());
+        workedHoursSpinner.setValueFactory(workHouredValueFactory);
+        SpinnerValueFactory<Integer> totalHoursValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE, chef.getTotalHours());
+        totalHoursSpinner.setValueFactory(totalHoursValueFactory);
+    }
+    private void setDataLabels(Waiter waiter){
+        idLabel.setText("Staff ID: "+waiter.getUserId());
+        userNameLabel.setText("Name: "+waiter.getFullName());
+        userTypeLabel.setText("User Type: "+waiter.getUserType());
+        SpinnerValueFactory<Integer> workHouredValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE, waiter.getHoursWorked());
+        workedHoursSpinner.setValueFactory(workHouredValueFactory);
+        SpinnerValueFactory<Integer> totalHoursValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE, waiter.getTotalHours());
+        totalHoursSpinner.setValueFactory(totalHoursValueFactory);
+    }
+    private void setDataLabels(Driver driver){
+        idLabel.setText("Staff ID: "+driver.getUserId());
+        userNameLabel.setText("Name: "+driver.getFullName());
+        userTypeLabel.setText("User Type: "+driver.getUserType());
+        SpinnerValueFactory<Integer> workHouredValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE, driver.getHoursWorked());
+        workedHoursSpinner.setValueFactory(workHouredValueFactory);
+        SpinnerValueFactory<Integer> totalHoursValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE, driver.getTotalHours());
+        totalHoursSpinner.setValueFactory(totalHoursValueFactory);
     }
 }
