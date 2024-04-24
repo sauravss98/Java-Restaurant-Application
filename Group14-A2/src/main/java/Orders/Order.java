d05ec3eaca911121ceb9cc847d85349a03bd8bf9
Saurav S <77258135+sauravss98@users.noreply.github.com>
2024-04-24 16:02:42 +0530

    added jdocs in order class

--- Group14-A2/src/main/java/Orders/Order.java
+++ Group14-A2/src/main/java/Orders/Order.java
@@ -5,6 +5,10 @@ import java.security.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * Class to create new object for the orders
+ * @author Saurav
+ */
 public class Order {
     private int orderId;
     private String orderType;
@@ -21,14 +25,26 @@ public class Order {
 
     private ArrayList<OrderItem> orderItems = new ArrayList<>();
 
+    /**
+     * Getter function for customer id
+     * @return customerID
+     */
     public int getCustomerId() {
         return customerId;
     }
 
+    /**
+     * Setter function for setting the customer id
+     * @param customerId the customer id is passed
+     */
     public void setCustomerId(int customerId) {
         this.customerId = customerId;
     }
 
+    /**
+     * Setter function for setting the items
+     * @param items the items is passed
+     */
     public void setItems(ArrayList<Integer> items) {
         this.items = items;
     }
@@ -37,77 +53,153 @@ public class Order {
         return itemsObjects;
     }
 
+    /**
+     * Setter function for setting the item objects(items in the order)
+     * @param itemsObjects the item objects is passed
+     */
     public void setItemsObjects(ArrayList<Item> itemsObjects) {
         this.itemsObjects = itemsObjects;
     }
 
+    /**
+     * Getter function for order status
+     * @return order status
+     */
     public String getOrderStatus() {
         return orderStatus;
     }
 
+    /**
+     * Setter function for setting the order status
+     * @param orderStatus the order status is passed
+     */
     public void setOrderStatus(String orderStatus) {
         this.orderStatus = orderStatus;
     }
 
+    /**
+     * Getter function for price
+     * @return price
+     */
     public int getPrice() {
         return price;
     }
 
+    /**
+     * Setter function for setting the price
+     * @param price the price is passed
+     */
     public void setPrice(int price) {
         this.price = price;
     }
 
+    /**
+     * Getter function for order data
+     * @return order date
+     */
     public Date getOrderDate() {
         return orderDate;
     }
 
+    /**
+     * Setter function for setting the order date
+     * @param orderDate the date is passed
+     */
     public void setOrderDate(Date orderDate) {
         this.orderDate = orderDate;
     }
 
+    /**
+     * Getter function for order time
+     * @return order time
+     */
     public Timestamp getOrderCreatedTime() {
         return orderCreatedTime;
     }
 
+    /**
+     * Setter function for setting the order created time
+     * @param orderCreatedTime the time is passed
+     */
     public void setOrderCreatedTime(Timestamp orderCreatedTime) {
         this.orderCreatedTime = orderCreatedTime;
     }
 
+    /**
+     * Getter function for order completed time
+     * @return time
+     */
     public Timestamp getOrderCompletedTime() {
         return orderCompletedTime;
     }
 
+    /**
+     * Setter function for setting the order complete time
+     * @param orderCompletedTime the time is passed
+     */
     public void setOrderCompletedTime(Timestamp orderCompletedTime) {
         this.orderCompletedTime = orderCompletedTime;
     }
 
+    /**
+     * Constructor for creating empty order
+     */
     public Order() {
     }
 
+    /**
+     * Getter function for order id
+     * @return order id
+     */
     public int getOrderId() {
         return orderId;
     }
 
+    /**
+     * Setter function for setting the order id
+     * @param orderId the order id is passed
+     */
     public void setOrderId(int orderId) {
         this.orderId = orderId;
     }
 
+    /**
+     * Getter function for order type
+     * @return ordertype
+     */
     public String getOrderType() {
         return orderType;
     }
 
+    /**
+     * Setter function for setting the order type
+     * @param orderType the order type is passed
+     */
     public void setOrderType(String orderType) {
         this.orderType = orderType;
     }
 
+    /**
+     * Getter function for items list
+     * @return items
+     */
     public ArrayList<Integer> getItems() {
         return items;
     }
 
+    /**
+     * Function to add item to the item arraylist
+     * @param item the item is passed
+     */
     public void addToItem(Integer item){
         items.add(item);
     }
 
+    /**
+     * Function to set the items into the order
+     * @param itemId the item id
+     * @param item the item object
+     */
     public void setItems(int itemId, Item item) {
         items.add(itemId);
         boolean found = false;
@@ -123,6 +215,11 @@ public class Order {
         }
     }
 
+    /**
+     * Function to add item into the order item array list
+     * @param item item object
+     * @param quantity item quantity
+     */
     public void addItem(Item item, int quantity) {
         boolean found = false;
         for (OrderItem orderItem : orderItems) {
@@ -137,6 +234,10 @@ public class Order {
         }
     }
 
+    /**
+     * Function to remove item from the list
+     * @param itemId id of the item passed
+     */
     public void removeItem(int itemId) {
         orderItems.removeIf(orderItem -> orderItem.getItem().getItemID() == itemId);
         for (int i = items.size() - 1; i >= 0; i--) {
@@ -146,20 +247,43 @@ public class Order {
         }
     }
 
+    /**
+     * Getter function for order item
+     * @return order item
+     */
     public ArrayList<OrderItem> getOrderItems() {
         return orderItems;
     }
 
 
+    /**
+     * Getter function for completed status
+     * @return is completed status
+     */
     public boolean isCompleted() {
         return isCompleted;
     }
 
+    /**
+     * Setter function for setting the completion status
+     * @param completed the completion status is passed
+     */
     public void setCompleted(boolean completed) {
         isCompleted = completed;
     }
 
     // Customer direcly orders
+
+    /**
+     * Constructor for creating the order object
+     * This constructor is used by the customer to order
+     * @param orderId the order id is passed
+     * @param orderType the order type is passed
+     * @param items the items is passed
+     * @param isCompleted the completion status is passed
+     * @param orderStatus the order status is passed
+     * @param customerId the customer id is passed
+     */
     public Order(int orderId, String orderType, ArrayList<Integer> items, boolean isCompleted,String orderStatus, int customerId) {
         this.orderId = orderId;
         this.orderType = orderType;
@@ -171,15 +295,20 @@ public class Order {
     }
 
 //    @Override
-    public String toStringtest() {
-        return "Order{" +
-                "orderId=" + orderId +
-                ", orderType='" + orderType + '\'' +
-                ", items=" + items +
-                ", items=" + itemsObjects +
-                ", isCompleted=" + isCompleted +
-                '}';
-    }
+//    public String toStringtest() {
+//        return "Order{" +
+//                "orderId=" + orderId +
+//                ", orderType='" + orderType + '\'' +
+//                ", items=" + items +
+//                ", items=" + itemsObjects +
+//                ", isCompleted=" + isCompleted +
+//                '}';
+//    }
+
+    /**
+     * Function to return the order data as a string
+     * @return string containing all the order details
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("Order{");
@@ -198,6 +327,10 @@ public class Order {
         return sb.toString();
     }
 
+    /**
+     * Function to return the order details for the list views
+     * @return String with order data
+     */
     public String getDescriptionForOrderList() {
         return "ID: "+orderId+" Order Status : " + orderStatus + " - price: "+price;
     }
