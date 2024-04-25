930286c706491df34aee2c38a317e3400affe00e
Saurav S <77258135+sauravss98@users.noreply.github.com>
2024-04-25 17:11:06 +0530

    added comments to the 2 files

--- Group14-A2/src/main/java/Orders/OrderDetailsPageController.java
+++ Group14-A2/src/main/java/Orders/OrderDetailsPageController.java
@@ -4,25 +4,45 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.text.Text;
 
+/**
+ * Class to handle the order details view page
+ * @author Saurav
+ */
 public class OrderDetailsPageController {
     @FXML private Text orderIdText;
     @FXML private ListView OrderItemsList;
     private Order order;
 
+    /**
+     * Function to initialize the UI and fxml components
+     * @author Saurav
+     */
     public void initialize(){
         refreshOrderIDText();
         refreshItemList();
     }
+
+    /**
+     * Function to set the order data
+     * @param order Order object is passed
+     */
     public void setOrderData(Order order){
         this.order = order;
         System.out.println("Order is "+order.toString());
         initialize();
     }
 
+    /**
+     * Function to get the order data
+     * @return Returns the order object required
+     */
     public Order getOrderData(){
         return order;
     }
 
+    /**
+     * Function to refresh the order id text field
+     */
     public void refreshOrderIDText(){
         if (order != null) {
             orderIdText.setText("Order Id: " + order.getOrderId());
@@ -32,6 +52,9 @@ public class OrderDetailsPageController {
         }
     }
 
+    /**
+     * Function to refresh and render the item list
+     */
     public void refreshItemList() {
         if (order != null && order.getOrderItems() != null) {
             OrderItemsList.getItems().clear();
--- Group14-A2/src/main/java/Orders/OrderItem.java
+++ Group14-A2/src/main/java/Orders/OrderItem.java
@@ -2,27 +2,51 @@ package Orders;
 
 import Items.Item;
 
+/**
+ * Class to hold data about the items in the order
+ * @author Saurav
+ */
 public class OrderItem {
     private Item item;
     private int quantity;
 
+    /**
+     * Constructor to create an order item
+     * @param item The item object is passed
+     * @param quantity The quantity value is passed
+     */
     public OrderItem(Item item, int quantity) {
         this.item = item;
         this.quantity = quantity;
     }
 
+    /**
+     * Function to get the Item object
+     * @return Item object
+     */
     public Item getItem() {
         return item;
     }
 
+    /**
+     * Function to get the quantity value
+     * @return Returns the quantity of each item
+     */
     public int getQuantity() {
         return quantity;
     }
 
+    /**
+     * Function to set the quantity for the item
+     * @param quantity The quantity value is passed
+     */
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
+    /**
+     * Function to increment the quantity of the item by 1
+     */
     public void incrementQuantity() {
         this.quantity++;
     }
