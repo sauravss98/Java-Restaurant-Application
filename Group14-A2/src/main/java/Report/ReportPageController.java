9fb2240bda6fe8d74628e21b27f434064cb39e11
Saurav S <77258135+sauravss98@users.noreply.github.com>
2024-04-29 16:33:29 +0530

    report jdoc done

--- Group14-A2/src/main/java/Report/ReportPageController.java
+++ Group14-A2/src/main/java/Report/ReportPageController.java
@@ -9,14 +9,24 @@ import javafx.scene.control.ListView;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Class to control the report main page
+ * @author Saurav
+ */
 public class ReportPageController {
     @FXML private Button backButton;
     @FXML private Button generateReportButton;
     @FXML private ListView reportListView;
     private ArrayList<Report> reports = ReportGenerator.getReports();
 
+    /**
+     * Default constructor for the class
+     */
     public ReportPageController(){}
 
+    /**
+     * Function to initialize the UI components
+     */
     public void initialize(){
         refreshList();
         backButton.setOnAction(e->{
@@ -32,6 +42,9 @@ public class ReportPageController {
         });
     }
 
+    /**
+     * Function to refresh the report list view
+     */
     private void refreshList() {
         reportListView.getItems().clear();
         for (Report report : reports) {
@@ -39,6 +52,9 @@ public class ReportPageController {
         }
     }
 
+    /**
+     * Function to handle the generate button click and it creates a report as an Excel sheet
+     */
     private void handleGenerateButton() {
         ReportGenerator.generateReport();
     }
