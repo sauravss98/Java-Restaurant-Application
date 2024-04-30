7aeea75239e70bcff0acaaac4f4143a9e9217f57
Saurav S <77258135+sauravss98@users.noreply.github.com>
2024-04-30 22:07:25 +0100

    added jdocs to some more user files 9

--- Group14-A2/src/main/java/User/WaiterTimeLogController.java
+++ Group14-A2/src/main/java/User/WaiterTimeLogController.java
@@ -7,6 +7,10 @@ import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.stage.Stage;
 
+/**
+ * Class to handle the waiter time logger page
+ * @author Saurav
+ */
 public class WaiterTimeLogController {
     private static Waiter currentWaiter;
     private Stage stage;
@@ -16,22 +20,40 @@ public class WaiterTimeLogController {
     @FXML private Label warningLabel;
     private static int oldHoursWorked;
 
+    /**
+     * Default constructor for the class
+     */
     public WaiterTimeLogController(){}
 
+    /**
+     * Function to set the waiter instance to the class
+     * @param currentWaiter The waiter object is passed
+     */
     public void setCurrentWaiter(Waiter currentWaiter) {
         this.currentWaiter = currentWaiter;
         setOldHoursWorked(currentWaiter.getHoursWorked());
         initialize();
     }
 
+    /**
+     * Function to set the old hours worked value to compare if there is change in the value
+     * @param oldHoursWorked
+     */
     public void setOldHoursWorked(int oldHoursWorked) {
         this.oldHoursWorked = oldHoursWorked;
     }
 
+    /**
+     * Function to set the stage object to the class
+     * @param stage The stage object is passed
+     */
     public void setStage(Stage stage) {
         this.stage = stage;
     }
 
+    /**
+     * Function to initialize the UI components
+     */
     public void initialize(){
         warningLabel.setVisible(false);
         backButton.setOnAction(e->{
@@ -43,6 +65,9 @@ public class WaiterTimeLogController {
         refreshSpinner();
     }
 
+    /**
+     * Function to refresh the spinner value
+     */
     private void refreshSpinner() {
             if (currentWaiter != null) {
                 SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE, currentWaiter.getHoursWorked());
@@ -53,6 +78,9 @@ public class WaiterTimeLogController {
             }
     }
 
+    /**
+     * Function to handle the confirm button click and save data
+     */
     private void handleConfirmClick() {
         if((int)timeSpinner.getValue()>oldHoursWorked){
             currentWaiter.setHoursWorked((int)timeSpinner.getValue());
@@ -67,6 +95,9 @@ public class WaiterTimeLogController {
         }
     }
 
+    /**
+     * Function to handle the back button click
+     */
     private void handleBackClick() {
         if(stage!=null){
             stage.close();
