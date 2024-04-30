1b091ba74aae56d3ce77b662f7f9968129d75bee
Saurav S <77258135+sauravss98@users.noreply.github.com>
2024-04-30 17:51:45 +0100

    added jdocs to some more user files

--- Group14-A2/src/main/java/User/Customer.java
+++ Group14-A2/src/main/java/User/Customer.java
@@ -1,5 +1,9 @@
 package User;
 
+/**
+ * Class for creating instance of customer
+ * @author Saurav
+ */
 public class Customer extends User{
     private int customerID;
     private String address;
@@ -11,10 +15,18 @@ public class Customer extends User{
         this.isCustomer = isCustomer;
     }
 
+    /**
+     * Function to get the customer id
+     * @return Customer id is returned
+     */
     public int getCustomerID() {
         return customerID;
     }
 
+    /**
+     * Function to display details of customers where required
+     * @return
+     */
     @Override
     public String toString() {
         return "Customer{" +
@@ -24,14 +36,26 @@ public class Customer extends User{
                 '}'+super.toString();
     }
 
+    /**
+     * Function to return the address
+     * @return Returns the address as string
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Function to set the customer id
+     * @param customerID Passed the id as integer
+     */
     public void setCustomerID(int customerID) {
         this.customerID = customerID;
     }
 
+    /**
+     * Function to set the address to the object
+     * @param address Address is passed as string
+     */
     public void setAddress(String address) {
         this.address = address;
     }
--- Group14-A2/src/main/java/User/Driver.java
+++ Group14-A2/src/main/java/User/Driver.java
@@ -1,22 +1,22 @@
 package User;
 
+/**
+ * Class to create instance of the driver
+ * @author Saurav
+ */
 public class Driver extends Staff{
     private boolean isDriver = true;
-    private boolean isDelivered;
 
+    /**
+     * Function to get if user is driver
+     * @return Boolean value.True if driver
+     */
     public boolean getIsDriver() {
         return isDriver;
-    }
-    public boolean getIsDelivered() {
-        return isDelivered;
     }
 
     public Driver(int userId, String email, String firstName, String lastName, int staffID, int hoursWorked, int totalHours, boolean isStaff, boolean isDriver,String userType,boolean isLoggedIn,boolean isActive) {
         super(userId, email, firstName, lastName, staffID, hoursWorked, totalHours, isStaff,userType,isLoggedIn,isActive);
         this.isDriver = isDriver;
-    }
-
-    public void setDelivered(boolean delivered) {
-        isDelivered = delivered;
     }
 }
