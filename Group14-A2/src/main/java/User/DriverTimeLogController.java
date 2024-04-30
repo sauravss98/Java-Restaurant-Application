d224da4b668b48701954d8263b001ba5311d4b37
Saurav S <77258135+sauravss98@users.noreply.github.com>
2024-04-30 19:20:53 +0100

    added jdocs to some more user files 4

--- Group14-A2/src/main/java/User/DriverTimeLogController.java
+++ Group14-A2/src/main/java/User/DriverTimeLogController.java
@@ -7,32 +7,53 @@ import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.stage.Stage;
 
+/**
+ * Class to control the driver time log page
+ * @author Saurav
+ */
 public class DriverTimeLogController {
     private static Driver currentDriver;
     private Stage stage;
-    @FXML
-    private Button backButton;
+    @FXML private Button backButton;
     @FXML private Button confirmButton;
     @FXML private Spinner timeSpinner;
     @FXML private Label warningLabel;
     private static int oldHoursWorked;
 
+    /**
+     * Default Constructor for the class
+     */
     public DriverTimeLogController(){}
 
+    /**
+     * Function to set the current driver
+     * @param currentDriver Driver object is passed
+     */
     public void setCurrentDriver(Driver currentDriver) {
         this.currentDriver = currentDriver;
         setOldHoursWorked(currentDriver.getHoursWorked());
         initialize();
     }
 
+    /**
+     * Function to set the old hours worked to compare the difference
+     * @param oldHoursWorked Time is passed as integer
+     */
     public void setOldHoursWorked(int oldHoursWorked) {
         this.oldHoursWorked = oldHoursWorked;
     }
 
+    /**
+     * Function to set stage to the instance
+     * @param stage The stage is passed
+     */
     public void setStage(Stage stage) {
         this.stage = stage;
     }
 
+    /**
+     * Function to initialize the UI components when fxml is loaded
+     */
     public void initialize(){
         warningLabel.setVisible(false);
         backButton.setOnAction(e->{
@@ -44,6 +65,9 @@ public class DriverTimeLogController {
         refreshSpinner();
     }
 
+    /**
+     * Function to refresh the spinner
+     */
     private void refreshSpinner() {
         if (currentDriver != null) {
             SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE, currentDriver.getHoursWorked());
@@ -54,6 +78,9 @@ public class DriverTimeLogController {
         }
     }
 
+    /**
+     * Function to handle the confirm button click and save the data
+     */
     private void handleConfirmClick() {
         if((int)timeSpinner.getValue()>oldHoursWorked){
             currentDriver.setHoursWorked((int)timeSpinner.getValue());
@@ -68,6 +95,9 @@ public class DriverTimeLogController {
         }
     }
 
+    /**
+     * Function to handle the back button click
+     */
     private void handleBackClick() {
         if(stage!=null){
             stage.close();
--- Group14-A2/src/main/java/User/Manager.java
+++ Group14-A2/src/main/java/User/Manager.java
@@ -1,11 +1,30 @@
 package User;
 
+/**
+ * Class to create instance of manager
+ * @author Saurav
+ */
 public class Manager extends Staff{
     private boolean isManager = true;
     public boolean getIsManager() {
         return isManager;
     }
 
+    /**
+     * Constructor to create new instance of manager class
+     * @param userId User id as integer is passed
+     * @param email Email as string is passed
+     * @param firstName First name as string is passed
+     * @param lastName Last name as string is passed
+     * @param staffID Staff id as int is passed
+     * @param hoursWorked Hours worked as int is passed
+     * @param totalHours Total hours to worked as int is passed
+     * @param isStaff Staff status as boolean is passed
+     * @param isManager Manager status as boolean is passed
+     * @param userType User type is saved as string is passed
+     * @param isLoggedIn Login status is saved as boolean is passed
+     * @param isActive User active status is saved as boolean is passed
+     */
     public Manager(int userId, String email, String firstName, String lastName, int staffID, int hoursWorked, int totalHours, boolean isStaff, boolean isManager,String userType,boolean isLoggedIn,boolean isActive) {
         super(userId, email, firstName, lastName, staffID, hoursWorked, totalHours, isStaff,userType,isLoggedIn,isActive);
         this.isManager = isManager;
