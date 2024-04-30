3263e51b5693ab6c8396956d56647bf989338806
Saurav S <77258135+sauravss98@users.noreply.github.com>
2024-04-30 22:00:33 +0100

    added jdocs to some more user files 8

--- Group14-A2/src/main/java/User/Waiter.java
+++ Group14-A2/src/main/java/User/Waiter.java
@@ -1,21 +1,35 @@
 package User;
 
+/**
+ * Class to create new instance of the waiter class
+ * @author Saurav
+ */
 public class Waiter extends Staff{
-    private boolean orderTaken = false;
     private boolean isWaiter = true;
 
+    /**
+     * Function to get if user type is waiter
+     * @return Boolean value is returned
+     */
     public boolean getIsWaiter() {
         return isWaiter;
     }
 
-    public boolean getIsOrderTaken() {
-        return orderTaken;
-    }
-
-    public void setOrderTaken(boolean orderTaken) {
-        this.orderTaken = orderTaken;
-    }
-
+    /**
+     * Constructor to create new instance of waiter class
+     * @param userId User id as integer is passed
+     * @param email Email as string is passed
+     * @param firstName First name as string is passed
+     * @param lastName Last name as string is passed
+     * @param staffID Staff id as int is passed
+     * @param hoursWorked Hours worked as int is passed
+     * @param totalHours Total hours to worked as int is passed
+     * @param isStaff Staff status as boolean is passed
+     * @param isWaiter Waiter status as boolean is passed
+     * @param userType User type is saved as string is passed
+     * @param isLoggedIn Login status is saved as boolean is passed
+     * @param isActive User active status is saved as boolean is passed
+     */
     public Waiter(int userId, String email, String firstName, String lastName, int staffID, int hoursWorked, int totalHours, boolean isStaff,boolean isWaiter,String userType,boolean isLoggedIn,boolean isActive) {
         super(userId, email, firstName, lastName, staffID, hoursWorked, totalHours, isStaff,userType,isLoggedIn,isActive);
         this.isWaiter = isWaiter;
--- Group14-A2/src/main/java/User/WaiterMainPageController.java
+++ Group14-A2/src/main/java/User/WaiterMainPageController.java
@@ -15,6 +15,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Class to control the waiter main page
+ * @author Saurav
+ */
 public class WaiterMainPageController {
     private static String activeEmail;
     private static Waiter activeWaiter;
@@ -26,8 +30,15 @@ public class WaiterMainPageController {
     @FXML private Button logoutButton;
     @FXML private Button reservationButton;
 
+    /**
+     * Default Constructor
+     */
     public WaiterMainPageController(){}
 
+    /**
+     * Constructor to instantiate the class with waiter data
+     * @param email The email is passed as string
+     */
     public WaiterMainPageController(String email){
         this.activeEmail = email;
         for (Waiter waiter: waiters){
@@ -38,10 +49,17 @@ public class WaiterMainPageController {
         setActiveEmail(email);
     }
 
+    /**
+     * Function to set the active email to the class
+     * @param email The email is passed as string
+     */
     private void setActiveEmail(String email) {
         this.activeEmail = email;
     }
 
+    /**
+     * Function to initialize the UI component
+     */
     public void initialize(){
         makeOrderButton.setOnAction(e->{
             handleMakeOrderButtonClick();
@@ -68,6 +86,9 @@ public class WaiterMainPageController {
         });
     }
 
+    /**
+     * Function the handle the reservation click and switch the page to the reservation page
+     */
     private void handleReservationClick() {
         new WaiterReservationMainPageController(activeWaiter.getEmail());
         try {
@@ -77,6 +98,9 @@ public class WaiterMainPageController {
         }
     }
 
+    /**
+     * Function to logout the user and go to the login page
+     */
     private void handleLogoutButtonClick() {
         try {
             Main.setRoot("login");
@@ -85,6 +109,10 @@ public class WaiterMainPageController {
         }
     }
 
+    /**
+     * Function to switch to the time log page
+     * @throws IOException The exception is thrown if there is issue loading the fxml file
+     */
     private void handleLogTimeClick() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/cafe94/group14a2/waiterTimeLogger.fxml"));
         Parent root = loader.load();
@@ -99,6 +127,10 @@ public class WaiterMainPageController {
         orderViewStage.showAndWait();
     }
 
+    /**
+     * Function to switch to the order list page
+     * @throws IOException The exception is thrown if there is issue loading the fxml file
+     */
     private void handleCheckOrderButtonClick() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/cafe94/group14a2/waiterOrderView.fxml"));
         Parent root = loader.load();
@@ -113,6 +145,9 @@ public class WaiterMainPageController {
         orderViewStage.showAndWait();
     }
 
+    /**
+     * Function to switch to the order create page
+     */
     private void handleMakeOrderButtonClick() {
         new WaiterOrderController(activeWaiter);
         try {
